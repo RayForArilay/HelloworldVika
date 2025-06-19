3012b2397350149df5758e9546b5b8f23a50ad8f
RayForArilay <rayforarilay@gmail.com>
2025-06-20 02:18:34 +0300

    Initial Commit

---
+++ HelloworldVika/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloworldVika
+//
+//  Created by Виктория Струсь on 20.06.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
